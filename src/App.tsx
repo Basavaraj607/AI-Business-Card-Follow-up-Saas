088ffb4028e5efb7db2c8d9f150b1ff0395fb3af
Basavaraj607 <Basavarajpalled607@gmail.com>
2026-06-21 13:35:53 +0530

    add the manage Events

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import { AuthProvider } from './lib/auth-context'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 import { LoginPage } from './pages/Login_Page'
 import { DashboardPage } from './pages/DashboardPage'
 import { ContactsPage } from './pages/ContactsPage'
@@ -8,17 +11,21 @@ import { ContactDetailPage } from './pages/ContactDetailPage'
 import { UploadPage } from './pages/UploadPage'
 import { SettingsPage } from './pages/SettingsPage'
 import { FollowupsPage } from './pages/FollowupsPage'
+import { EventsPage } from './pages/EventsPage'
 import { AdminOverviewPage } from './pages/admin/AdminOverviewPage'
 import { AdminTenantsPage } from './pages/admin/AdminTenantsPage'
 import { AdminTenantDetailPage } from './pages/admin/AdminTenantDetailPage'
 import { AdminUsersPage } from './pages/admin/AdminUsersPage'
+import { AdminEventsPage } from './pages/admin/AdminEventsPage'
+import { AdminEventRegistrationsPage } from './pages/admin/AdminEventRegistrationsPage'
 import AppLayout from './layouts/AppLayout'
 import ProtectedRoute from './routes/ProtectedRoute'
 
 export function App() {
   return (
-    <AuthProvider>
-      <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <BrowserRouter>
         <Routes>
           <Route path="/login" element={<LoginPage />} />
           
@@ -29,7 +36,12 @@ export function App() {
             <Route path="/contacts/:id" element={<ContactDetailPage />} />
             <Route path="/contacts/upload" element={<UploadPage />} />
             <Route path="/followups" element={<FollowupsPage />} />
+            <Route path="/events" element={<EventsPage />} />
             <Route path="/settings" element={<SettingsPage />} />
+            
+            {/* Admin Event Management */}
+            <Route path="/admin/events" element={<AdminEventsPage />} />
+            <Route path="/admin/events/:id/registrations" element={<AdminEventRegistrationsPage />} />
             
             {/* Super Admin Console */}
             <Route path="/admin" element={<ProtectedRoute requireAdmin={true}><AdminOverviewPage /></ProtectedRoute>} />
@@ -43,6 +55,7 @@ export function App() {
         </Routes>
       </BrowserRouter>
     </AuthProvider>
+    </QueryClientProvider>
   )
 }
 
